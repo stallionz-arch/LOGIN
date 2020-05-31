b48c3541efec870a9599dbbe9faee26d711f14d8
Sayan Mondal <sayan.mondal.tech@gmail.com>
2020-05-31 16:49:12 +0530

    questions app

--- inquest/inquest/urls.py
+++ inquest/inquest/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from home import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('web/', views.web, name='web'),
     path('index/', views.index, name='index'),
+    path('blog/', include('blog.urls'), name='blog'),
 
 ]
 
